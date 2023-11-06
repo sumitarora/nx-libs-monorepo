cdfeb42c24d39c2076c8475dfa0572198ec93443
Sumit Arora <er.sumitarora@gmail.com>
2023-11-06 13:50:30 -0500

    feat: updating packages

--- libs/check-number/src/lib/check-number.ts
+++ libs/check-number/src/lib/check-number.ts
@@ -3,7 +3,7 @@ import { isOdd } from '@er.sumitarora/is-odd';
 import { CheckNumberResponse } from '@er.sumitarora/models';
 
 export function checkNumber(input: number): CheckNumberResponse {
-  console.log('change - 3');
+  console.log('change - 4');
   const isEvenNumber = isEven(input);
   const isOddNumber = isOdd(input);
   return {
